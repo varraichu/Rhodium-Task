d09e737e87afbaf42136b663fec2e5f7e5de5004
varraichu <qasimanwarv5@gmail.com>
2025-01-15 21:23:02 +0500

    redeploy

--- backend/server.js
+++ backend/server.js
@@ -58,6 +58,6 @@ app.get('/s3Url', async (req, res) => {
 
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
